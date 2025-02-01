dcbe36d013b71cfe38a000be992c8c7c46acf9fc
Jin Xuan Ow Yong <87897838+jinxuan-owyong@users.noreply.github.com>
2025-02-01 11:31:07 +0800

    refactor: rename variables and add comments

--- go/3152/3152_SpecialArrayII.go
+++ go/3152/3152_SpecialArrayII.go
@@ -18,18 +18,25 @@ func isArraySpecial(nums []int, queries [][]int) []bool {
 		precompute the longest consecutive special range for each starting position
 		a query is valid if its ending index is <= the value calculated above
 	*/
-	end := 0
+	right := 0
 	valid := []int{}
 
-	for curr := range nums {
-		if curr > end {
-			end = curr
+	for left := range nums {
+		// special window shrinks to zero, then we find the next window using left as the new starting position
+		/*
+		* * [     ] * * *
+		* * * [   ] * * *
+		* * * * [ ] * * *
+		* * * * * [ ] * *
+		 */
+		if left > right {
+			right = left
 		}
 		// increment end until before it results in an invalid range
-		for end < len(nums)-1 && nums[end]%2 != nums[end+1]%2 {
-			end += 1
+		for right < len(nums)-1 && nums[right]%2 != nums[right+1]%2 {
+			right += 1
 		}
-		valid = append(valid, end)
+		valid = append(valid, right)
 	}
 
 	result := []bool{}
