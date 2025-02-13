3a80e62a6d4f9cd45182387aa11b72040b1bf2f2
Jin Xuan Ow Yong <87897838+jinxuan-owyong@users.noreply.github.com>
2025-02-13 23:13:29 +0800

    Blind 75 - 128. Longest Consecutive Sequence

--- py/0128_LongestConsecutiveSequence.py
+++ py/0128_LongestConsecutiveSequence.py
@@ -1,80 +1,39 @@
 # 128. Longest Consecutive Sequence
 
+from utils import chunk
 from typing import List
-
-
-class UnionFind:
-    def __init__(self, n: int) -> None:
-        self.parent = [i for i in range(n)]
-        self.size = {i: 1 for i in range(n)}
-
-    def find(self, x: int) -> int:
-        if self.parent[x] == x:
-            return x
-        # path compression
-        self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x: int, y: int) -> None:
-        rootX = self.find(x)
-        rootY = self.find(y)
-
-        if rootX == rootY:
-            return
-
-        # join the shorter tree to the taller tree
-        if self.size[x] > self.size[y]:
-            self.parent[rootY] = rootX
-            self.size[rootX] += self.size[rootY]
-            self.size[rootY] = 0
-        else:
-            self.parent[rootX] = rootY
-            self.size[rootY] += self.size[rootX]
-            self.size[rootX] = 0
 
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        """
+        trivial: sort the array and scan
+        using a hash set, n is a valid start value iff n-1 does not exist in the set
+         -> this is always true if n is a non-start value
+        if valid start value, then scan the array for consecutive size
+        """
+        longest = 0
+        values = set(nums)
+        checked = set()
 
-        uf = UnionFind(len(nums))
-        numsIdx = {}
+        for n in nums:
+            if n-1 not in values and n not in checked:
+                checked.add(n)
+                count = 0
+                while n in values:
+                    count += 1
+                    n += 1
+                if count > longest:
+                    longest = count
 
-        for i in range(len(nums)):
-            if nums[i] in numsIdx:
-                continue
-            if nums[i] - 1 in numsIdx:
-                uf.union(i, numsIdx[nums[i] - 1])
-            if nums[i] + 1 in numsIdx:
-                uf.union(i, numsIdx[nums[i] + 1])
-            numsIdx[nums[i]] = i
-
-        return max(uf.size.values())
+        return longest
 
 
 if __name__ == "__main__":
+    testSize = 1
     puzzles = [
         [100, 4, 200, 1, 3, 2],
-        [0, 3, 7, 2, 5, 8, 4, 6, 0, 1],
-        [],
-        [0, - 1]
+        [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
     ]
-    for puzzle in puzzles:
-        print(Solution().longestConsecutive(puzzle))
-
-"""
-Runtime
-526
-ms
-Beats
-39.20%
-of users with Python3
-Memory
-45.74
-MB
-Beats
-5.01%
-of users with Python3
-40*
-"""
+    for puzzle in chunk(puzzles, testSize):
+        print(Solution().longestConsecutive(*puzzle))
