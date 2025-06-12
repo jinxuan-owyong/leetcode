226285acc9ea92755571467e4cdd5014a2ae73cf
Jin Xuan Ow Yong <87897838+jinxuan-owyong@users.noreply.github.com>
2025-06-12 13:07:18 +0800

    NeetCode 150 - 567. Permutation in String

--- py/0567_PermutationinString.py
+++ py/0567_PermutationinString.py
@@ -7,36 +7,66 @@ from collections import defaultdict
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # to find if s2 contains a permutation of s1
-        i, j = 0, 0
-        valid, curr = Counter(s1), defaultdict(int)
+        # using a fixed size sliding window, we can check if a permutation of
+        # s1 exists in s2
+        # after every iteration, perform O(26) check if permutation exists -> O(26*N)
+        if len(s1) > len(s2):
+            return False
 
-        def decrementKey(key: str):
-            if key not in curr:
-                return
-            if curr[key] == 1:
-                del curr[key]
-            else:
-                curr[key] -= 1
+        # O(26) check can be optimised by keeping track of the number of matched chars in window
+        # each time the count in have "passes" the count in want, we in/decrement accordingly
+        # the number will only change when this threshold is hit
+        # then the string is a permutation when matches is equal to number of unique chars in s1
+        matches = 0
+        required = len(set(s1))
+        want = Counter(s1)
+        have = defaultdict(int)
+        i = 0
+        for j in range(len(s2)):  # O(N)
+            have[s2[j]] += 1
+            if have[s2[j]] == want[s2[j]]:
+                matches += 1
 
-        while j < len(s2):
-            # reset if incrementing a char causes it to be more than s1
-            if s2[j] not in valid or curr[s2[j]] == valid[s2[j]]:
-                decrementKey(s2[i])
+            # shrink window to size of s1
+            while (j-i+1) > len(s1):
+                if have[s2[i]] == want[s2[i]]:
+                    matches -= 1
+                have[s2[i]] -= 1
                 i += 1
-                if j < i:
-                    j = i
-                while i < j and s2[i] not in curr:
-                    decrementKey(s2[i])
-                    i += 1
-            else:
-                curr[s2[j]] += 1
-                j += 1
 
-            if valid == curr:
+            if matches == required:
                 return True
 
         return False
+
+# class Solution:
+#     def checkInclusion(self, s1: str, s2: str) -> bool:
+#         # using a fixed size sliding window, we can check if a permutation of
+#         # s1 exists in s2
+#         # after every iteration, perform O(26) check if permutation exists -> O(26*N)
+#         if len(s1) > len(s2):
+#             return False
+
+#         want = Counter(s1)
+#         have = defaultdict(int)
+#         i = 0
+#         for j in range(len(s2)): # O(N)
+#             have[s2[j]] += 1
+
+#             # shrink window to size of s1
+#             while (j-i+1) > len(s1):
+#                 have[s2[i]] -= 1
+#                 i += 1
+
+#             hasPermutation = True
+#             for c in want: # O(26)
+#                 if have[c] < want[c]:
+#                     hasPermutation = False
+
+#             if hasPermutation:
+#                 return True
+
+#         return False
 
 
 if __name__ == "__main__":
