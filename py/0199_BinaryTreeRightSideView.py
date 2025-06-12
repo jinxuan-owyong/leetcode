5f0016cf74c26a223135fce3799af215dcadf0da
Jin Xuan Ow Yong <87897838+jinxuan-owyong@users.noreply.github.com>
2025-06-12 21:11:07 +0800

    NeetCode 150 - 199. Binary Tree Right Side View

--- py/0199_BinaryTreeRightSideView.py
+++ py/0199_BinaryTreeRightSideView.py
@@ -8,23 +8,19 @@ from collections import deque
 
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-
+        # level order traversal to get the right most element, can be in left subtree
         result = []
-        queue = deque([(root, 1)])  # node, level
-
-        # level-order traversal
-        while queue:
-            node, level = queue.popleft()
-            # last in queue or last in level
-            if not queue or queue[0][1] == level + 1:
-                result.append(node.val)
-            if node.left:
-                queue.append((node.left, level + 1))
-            if node.right:
-                queue.append((node.right, level + 1))
-
+        q = deque([root])
+        while q:
+            right = None
+            for _ in range(len(q)):
+                curr = q.popleft()
+                if not curr:
+                    continue
+                right = curr.val
+                q.extend([curr.left, curr.right])
+            if right != None:
+                result.append(right)
         return result
 
 
@@ -32,7 +28,8 @@ if __name__ == "__main__":
     puzzles = [
         [1, 2, 3],
         [1, 2, 3, 4, 5, 6, 7],
-        [1, 2]
+        [1, 2],
+        [0, 1, 2, None, 3, 4, None, None, 5, 9, None, None, 6, 10, None]
     ]
     for puzzle in puzzles:
         print(Solution().rightSideView(toTree(puzzle)))
